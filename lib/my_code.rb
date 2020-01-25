0bb41ab72fecbd293d05bb63d14fb53b6b60c38b
Daniel Phillips <danphi@gmail.com>
2020-01-25 19:02:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,11 +23,27 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
+  arr = []
   
+  i = 0
+  while i < source_array.length do
+    arr << source_array[i] * 2
+    i += 1
+  end
+  
+  arr
 end
 
 def map_to_square(source_array)
+  arr = []
   
+  i = 0
+  while i < source_array.length do
+    arr << source_array[i] * source_array[i]
+    i += 1
+  end
+  
+  arr
 end
 
 def reduce_to_total(source_array)
